ac4e81b4ba687be5d34c6b2275844bf9a8bb2dd5
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-26 20:09:26 +0100

    added. base strcture of homePage and define calendar props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,39 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
+import CalendarComponent from "../../components/my/CalendarComponent";
 export default function HomeScreen() {
+  const formatISO = (d: Date | undefined) =>
+    d ? d.toISOString().split("T")[0] : undefined;
+
+  const startDate: Date | undefined = new Date();
+  const formattedStartDate = formatISO(startDate);
+  const endDate: Date | undefined = undefined;
+  const formattedEndDate = formatISO(endDate);
+  const [month, setMonth] = useState<number>(0);
+
+  const colorBack: string = "#fbb5b5ff";
+  const colorDot: string = "#6bfaeeff";
+  const colorNumber: string = "#d6fd82ff";
+  const onSelected = (
+    dates: [string | undefined, string | undefined]
+  ): boolean => {
+    console.log("Dati:", dates);
+    return true;
+  };
+  const onChangeMonth = (m: number): void => {
+    setMonth(m);
+  };
   return (
     <View>
-      <Text>Sono Home</Text>
+      <CalendarComponent
+        startDate={startDate}
+        endDate={endDate}
+        colorBack={colorBack}
+        colorDot={colorDot}
+        colorNumber={colorNumber}
+        onSelected={() => onSelected([formattedStartDate, formattedEndDate])}
+        onChangeMonth={() => onChangeMonth(month)}
+      />
     </View>
   );
 }
--- components/my/CalendarComponent.tsx
+++ components/my/CalendarComponent.tsx
@@ -1,6 +1,24 @@
 import { Text, View } from "react-native";
 
-export default function Calendar() {
+type CalendarProps = {
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+
+  colorBack: string;
+  colorDot: string;
+  colorNumber: string;
+  onSelected: () => boolean;
+  onChangeMonth: () => void;
+};
+export default function Calendar({
+  startDate,
+  endDate,
+  colorBack,
+  colorDot,
+  colorNumber,
+  onSelected,
+  onChangeMonth,
+}: CalendarProps) {
   return (
     <View>
       <Text>Calendar</Text>
